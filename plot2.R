692b2db93dcd0b2b6a3e58f0c51f6001f3f540ff
Radu-Ionel Toma <57911302+radutoma-upt@users.noreply.github.com>
2020-11-02 20:17:25 +0200

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -1,22 +1,44 @@
-#load the data
+# Exploratory Data Analysis - Course Project 1
+# Plot 2
+
+# read in data files
+
+# Initialize variables
 rm(list = ls())
-data <- read.table("household_power_consumption.txt", header = T,
-                   sep = ";", na.strings = "?")
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fileArchiveName <- 'household_power_consumption.zip'
+fileName <- 'household_power_consumption.txt'
+
+# Download the dataset
+
+# Checking if archive already exists.
+if(!file.exists(fileArchiveName)) {
+        # If the file does not exist download it
+        download.file(fileURL, fileArchiveName, method="curl")
+}
+# Unzip the file
+unzip(fileArchiveName)
+
+#load the data
+data <- read.table(fileName, header = T, sep = ";", na.strings = "?")
 
 #convert the date variable to Date class
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 #Subset the data
-data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+dataSubset <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 #Convert dates and times
-data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+dataSubset$datetime <- strptime(paste(dataSubset$Date, dataSubset$Time), "%Y-%m-%d %H:%M:%S")
 
 #Plot 2
-data$datetime <- as.POSIXct(data$datetime)
-attach(data)
+dataSubset$datetime <- as.POSIXct(dataSubset$datetime)
+attach(dataSubset)
 plot(Global_active_power ~ datetime, type = "l",
      ylab = "Global Active Power (kilowatts)", xlab = "")
+
+# export the plot to a png file
 dev.copy(png, file = "plot2.png", height = 480, width = 480)
 dev.off()
-detach(data)
+detach(dataSubset)
